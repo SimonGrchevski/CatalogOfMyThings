bee1bec5252bb32e79fc66055cd8c42d46ff32a1
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-18 15:38:36 +0200

    [Item] Add #move_to_archive && can_be_archived?

--- item.rb
+++ item.rb
@@ -1,7 +1,9 @@
-class Item 
+require 'date'
+
+class Item
   attr_reader :id, :publish_date, :archieved
 
-  def initialize(id,publish_date,archieved)
+  def initialize(id, publish_date, archieved)
     @id = id
     @publish_date = publish_date
     @archieved = archieved
@@ -22,4 +24,14 @@ class Item
     @label.items self
   end
 
+  def move_to_archive
+    @archieved = can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    years_elapsed = Time.now.year - Date.parse('2020-10-18').year
+    years_elapsed > 10
+  end
 end

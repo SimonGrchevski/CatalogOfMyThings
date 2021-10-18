feb7e6b8a8c8c2b052ae40e999b0bc228dd7531d
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-10-18 21:11:58 +0200

    Add MusicAlbum class

---
+++ music_album.rb
@@ -0,0 +1,9 @@
+require_relative './item'
+
+class MusicAlbum < Item
+  def initialize(genre:, label:, author:, publish_date:, archieved:, on_spotify:)
+    super(genre: genre, label: label, author: author, publish_date: publish_date, archieved: archieved)
+
+    @on_spotify = on_spotify
+  end
+end
